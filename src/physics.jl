0db896fe0844fbb75815d6457a02a4b22790b277
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-04-12 18:04:12 +0200

    add: elliptic curvature potential (with code refactoring)

--- src/physics.jl
+++ src/physics.jl
@@ -4,6 +4,7 @@ struct Simulation
     S::Float64
     ds::Float64
     equation::String #("GPE", "NPSE")
+    potential::String #("barrier", "ellipse")
 end
 
 
@@ -14,46 +15,89 @@ struct Apparatus
     N::Int64
     γ::Float64
     ω_z::Float64
-  end
-  
+    ϵ::Float64
+    a::Float64
+end
 
-  struct InitialState
+
+struct InitialState
     width::Float64 # m
     sech_flag::Float64 # 1 or 0
-  end
+end
 
 
- struct Coefficients
+struct Coefficients
     α::ComplexF64
     β::Function
     γ::Function
- end
+end
+
+function β(sim::Simulation, app::Apparatus, state::InitialState, s::Float64)
+    hbar = 6.62607015e-34 / (2 * pi)
+    l_perp = sqrt(hbar / (app.m * app.ω_perp))
+    l_z = sqrt(hbar / (app.m * app.ω_z))
+    if (sim.equation == "NPSE")
+        value = im * hbar / (8 * app.m)
+    elseif (sim.equation == "GPE")
+        value = -im * app.ω_perp
+    end
+
+    if sim.potential == "barrier"
+        value += -im * hbar * exp(-(s - 15e-6)^2 / (5e-6)^2) * 9e12 / (8 * app.m)
+    elseif sim.potential == "ellipse"
+        target_error = 0.01 * app.a
+        lower_ϕ = -pi
+        upper_ϕ = 3 * pi
+        let error
+            let midpoint
+                error = target_error+1
+                while error > target_error
+                    midpoint = (lower_ϕ + upper_ϕ) / 2
+                    display(midpoint)
+                    if s - app.a * E(midpoint, app.ϵ) > 0
+                        lower_ϕ = midpoint
+                    else
+                        upper_ϕ = midpoint
+                    end
+                    error = abs(s - app.a * E(midpoint, app.ϵ))
+                end
+                display(midpoint)
+                value +=  im *  hbar * (1 / app.a * (sqrt(1 - app.ϵ^2)) / (sin(midpoint)^2 + sqrt(1 - app.ϵ^2) * cos(midpoint)^2)^(3 / 2))^2 / (8*app.m)  * 150
+            end
+        end
+    end
+    return value
+end
+
+function γ(sim::Simulation, app::Apparatus, state::InitialState, ψ::ComplexF64)
+    hbar = 6.62607015e-34 / (2 * pi)
+    l_perp = sqrt(hbar / (app.m * app.ω_perp))
+    l_z = sqrt(hbar / (app.m * app.ω_z))
+    npse_gamma = app.γ * hbar * app.ω_perp / l_z^6 * 1e-4
+    σ(ψ::ComplexF64) = l_perp * sqrt(sqrt(1 + 2 * app.as * (app.N - 1) * abs.(ψ)^2))
+    if sim.equation == "NPSE"
+        value = -im * hbar / (2 * app.m * σ(ψ)^2) - im * app.m * app.ω_perp^2 / (2 * hbar) * σ(ψ)^2 - im * 2 * hbar * app.as * (app.N - 1) / (app.m * σ(ψ)^2) * abs.(ψ)^2 - npse_gamma * app.N^2 * abs.(ψ)^4
+    elseif sim.equation == "GPE"
+        value = -im * 2 * hbar * app.as * (app.N - 1) / (app.m * l_perp^2) * abs.(ψ)^2
+    end
+    return value
+end
 
 
 function get_coefficients(sim::Simulation, app::Apparatus, state::InitialState)
     # implement with PhysicalConstants.CODATA2018.h
-    hbar = 6.62607015e-34 / (2 * pi) 
+    hbar = 6.62607015e-34 / (2 * pi)
     l_perp = sqrt(hbar / (app.m * app.ω_perp))
     l_z = sqrt(hbar / (app.m * app.ω_z))
     npse_gamma = app.γ * hbar * app.ω_perp / l_z^6 * 1e-4
     @assert(sim.equation in ["NPSE", "GPE"])
-    if (sim.equation == "NPSE")
-        α = -im * hbar/(2*app.m)
-        β1(s) = im * hbar / (8*app.m)
-        σ(ψ::ComplexF64) = l_perp * sqrt(sqrt(1+2*app.as*(app.N - 1) * abs.(ψ)^2))
-        γ1(ψ::ComplexF64) = - im * hbar/ (2*app.m * σ(ψ)^2) - im * app.m * app.ω_perp^2 / (2*hbar) * σ(ψ)^2 - im * 2 * hbar * app.as * (app.N - 1)/(app.m * σ(ψ)^2) * abs.(ψ)^2 - npse_gamma * app.N^2 * abs.(ψ)^4
-        β=β1
-        γ=γ1
-    elseif (sim.equation == "GPE")
-        α = -im * hbar/(2*app.m) 
-        β2(s) = - im * app.ω_perp - im * hbar * exp(-(s-30e-6)^2/(5e-6)^2) *9e12 / (8*app.m)
-        #display( 2 * hbar * app.as * (app.N - 1) / (app.m * l_perp^2))
-        γ2(ψ::ComplexF64) = - im * 2 * hbar * app.as * (app.N - 1) / (app.m * l_perp^2) * abs.(ψ)^2
-        β=β2
-        γ=γ2
-    end
+    @assert(sim.potential in ["barrier", "ellipse", ""])
 
-    return Coefficients(α, β, γ)
+    α = -im * hbar / (2 * app.m)
+    beta(s::Float64) = β(sim::Simulation, app::Apparatus, state::InitialState, s)
+    gamma(ψ::ComplexF64) = γ(sim::Simulation, app::Apparatus, state::InitialState, ψ)
+
+    return Coefficients(α, beta, gamma)
 end
 
 
--- test/test_evolution.jl
+++ test/test_evolution.jl
@@ -1,54 +1,65 @@
 using SolitonBEC
 using Printf
-
+using Plots
+using Elliptic
 ### SIMULATE GENERATION OF SOLITON IN Li-7 CONDENSATE
 
 # --------- Simulation ---------
 khaykovich_gpe = Simulation(20e-3, #T
   5e-5, #dt
-  2e-4, #S
-  1e-7, #ds
-  "GPE") #ds
+  10e-6 * E(2*pi, 0.9), #S
+  10e-9 * E(2*pi, 0.9), #ds
+  "GPE",
+  "ellipse") #ds
 
 khaykovich_npse = Simulation(10e-3, #T
   1e-5, #dt
-  1e-4, #S
-  1e-7, #ds
-  "NPSE") #ds
+  10e-6 * E(2*pi, 0.9), #S
+  10e-9 * E(2*pi, 0.9), #ds
+  "NPSE",
+  "ellipse") #ds
 
 npse_sim = Simulation(300e-3, #T      ##exibits Talbot
   5e-4, #dt
-  100e-6, #S
-  1e-8, #ds
-  "NPSE") 
-  
+  10e-6 * E(2*pi, 0.9), #S
+  10e-9 * E(2*pi, 0.9), #ds
+  "NPSE",
+  "ellipse")
+
 # --------- Apparata ---------
 std_apparatus = Apparatus(6.941 * 1.660539e-27, #m (conversion AMU -> kg)
-               -0.21e-9, #as
-               2*pi*710, # ω_perp
-               4e3, #N
-               1.77e-11, # γ
-               2*pi*4) # ω_z
+  -0.21e-9, #as
+  2 * pi * 710, # ω_perp
+  4e3, #N
+  1.77e-11, # γ
+  2 * pi * 4, # ω_z
+  0.9,
+  10e-6)
 
 ## --------- InitialStates ---------   
-InitialState1 =  InitialState(1.7e-6,
-                0)    
+InitialState1 = InitialState(1.7e-6,
+  0)
 
 ## Configurations
-configs = [(khaykovich_gpe, std_apparatus, InitialState1), 
-         #(khaykovich_npse, std_apparatus, InitialState1), 
-         ]
+configs = [
+  (khaykovich_gpe, std_apparatus, InitialState1),
+  #(khaykovich_npse, std_apparatus, InitialState1), 
+]
 mem_limit = 15000000000 #byte
-cnt=1
+cnt = 1
+
+angle = LinRange(0, 2 * pi, 100)
+
+#plot(angle, -(sqrt(1 - 0.9^2) ./ (sin.(angle) .^ 2 + sqrt(1 - 0.9^2) * cos.(angle) .^ 2) .^ (3 / 2)) .^ 2, show=true)
 for (sim, app, state) in configs
   global cnt
   @printf("\n------Running simulation # %3i \n", cnt)
   estimate = mem_estimate(sim)
   if estimate < mem_limit
-    @printf("Estimated memory consumption: %4.1f MiB\n", estimate/(1024^2))
+    @printf("Estimated memory consumption: %4.1f MiB\n", estimate / (1024^2))
     @time run_simulation(sim, app, state)
   else
-    @printf("Estimated memory consumption (%4.1f MiB) exceed maximum!\n", estimate/(1024^2))
+    @printf("Estimated memory consumption (%4.1f MiB) exceed maximum!\n", estimate / (1024^2))
   end
-  cnt+=1
+  cnt += 1
 end
