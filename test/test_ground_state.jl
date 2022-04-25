240c174e3c3178fc8c4539ef11f970ca0b08953e
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-04-25 22:06:29 +0200

    add: spontaneous symmetry breaking

--- test/test_ground_state.jl
+++ test/test_ground_state.jl
@@ -40,7 +40,7 @@ end
 # --------- Apparata ---------
 std_apparatus = Apparatus(
   6.941 * 1.660539e-27, #m (conversion AMU -> kg)
-  -0.21e-9, # as
+  0, # as
   2 * pi * 710, # ω_perp
   4e3, #N
   1.77e-11, #γ
@@ -81,9 +81,9 @@ for (num, sim, pot, app, state) in configs
 end
 
 
-## Ground state--------------------------------------------
+## Non Interacting ground state--------------------------------------------
 
-fig2 = plot(title="stationary |ψ|^2",
+fig2 = plot(title="stationary density",
   xlabel="space [mm]",
   ylabel="ρ(s) * a",
   reuse=false)
@@ -113,6 +113,67 @@ for (num, sim, pot, app, state) in configs
 
     display(fig1)
     display(fig2)
+  else
+    @printf("Estimated memory consumption (%4.1f MiB) exceed maximum!\n", estimate / (1024^2))
+  end
+
+  cnt += 1
+
+end
+
+## Spontaneous Symmetry Breaking--------------------------------------------
+configs_gamma = []
+
+fixed_potential = elliptical(0.9)
+mass = 6.941 * 1.660539e-27
+ω_perp = 2 * pi * 710
+Num = 4e3
+
+as_apparatus(as) = Apparatus(
+  mass, #m (conversion AMU -> kg)
+  as, # as
+  ω_perp, # ω_perp
+  Num, #N
+  1.77e-11, #γ
+  2 * pi * 4,# ω_z
+)
+l_perp = sqrt(hbar / (mass * ω_perp))
+
+for gamma in [-10, -5, -1, -0.1, 0, 0.1, 1, 5, 10]
+  as = gamma * l_perp^2 / (2 * fixed_potential.a * (Num - 1))
+  display(as_apparatus(as))
+  push!(configs_gamma, (num, khaykovich_gpe, fixed_potential, as_apparatus(as), InitialState1))
+end
+
+fig3 = plot(title="stationary density",
+  xlabel="space [mm]",
+  ylabel="ρ(s) * a",
+  reuse=false)
+
+for (num, sim, pot, app, state) in configs_gamma
+  global cnt
+  @printf("\n------Running simulation # %3i \n", cnt)
+  estimate = mem_estimate(num)
+
+  if estimate < mem_limit
+
+    @printf("Estimated memory consumption: %4.1f MiB\n", estimate / (1024^2))
+
+    # Natural orientation choice for plot 
+    coeffs = get_coefficients(sim, app, pot, state)
+
+    time_steps = Int(floor(num.T / num.dt))
+    space_steps = Int(floor(num.S / num.ds))
+    time = LinRange(0, num.T, time_steps)
+    space = LinRange(-num.S / 2, num.S / 2, space_steps)
+
+    space, time, ψ = @time ground_state_solve(num, coeffs)
+    γ = (2 * pot.a * app.as * (app.N - 1)) / (l_perp^2)
+    plot!(fig3, space * 1e3,
+      abs.(ψ) .^ 2 * pot.a,
+      label="γ = $(γ)")
+
+    display(fig3)
   else
     @printf("Estimated memory consumption (%4.1f MiB) exceed maximum!\n", estimate / (1024^2))
   end
