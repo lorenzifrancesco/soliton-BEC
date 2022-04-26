831ec7b5884d7d42659477bb38e9b90af90dad5c
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-04-26 10:11:22 +0200

    fix: interaction g

--- test/test_collision.jl
+++ test/test_collision.jl
@@ -6,15 +6,16 @@ using Printf
 using Plots
 using Elliptic
 hbar = 6.62607015e-34 / (2 * pi)
+interaction_g = 2 / (5e-6)^2 # 2/width^2
 
 # --------- Numerics ---------
-L = 200e-6
+L = 100e-6
 
 num = Numerics(
-  50e-4, #T
-  5e-6, #dt
+  10e-4, #T
+  1e-6, #dt
   L, #S
-  L * 1e-4, #ds
+  L * 1e-3, #ds
 )
 
 
@@ -50,16 +51,20 @@ std_apparatus = Apparatus(
 )
 
 # --------- InitialStates ---------   
-velocity = 1.5e6
+width = sqrt(2 / interaction_g)
+
+velocity = 0.8 * interaction_g
+
 InitialState1 = InitialState(
   "sech", #type
-  5e-6, # width
-  velocity #v0
+  width, # width
+  -velocity #v0
 )
 
 ## Configurations
 configs = []
-for energy in LinRange(1e6, 40e6, 4)
+for energy_vs_g in LinRange(-0.5, -1.5, 4)
+  energy = interaction_g * energy_vs_g
   potential = barrier_height(energy)
   push!(configs, (num, khaykovich_gpe, std_apparatus, potential, InitialState1))
 end
@@ -69,15 +74,18 @@ mem_limit = 15000000000 #byte
 cnt = 1
 
 pyplot()
+plt_width = 800
+plt_height = 600
 p = Plots.palette(:rainbow_bgyr_35_85_c72_n256, length(configs) + 3)
 
 ## Soliton - barrier collision --------------------------------------------
-#@time run_dynamics(configs[2]...)
+@time run_dynamics(configs[2]...)
 
 fig1 = plot(title="|ψ|^2 after collision with barrier",
   xlabel="space [mm]",
   ylabel="|ψ|^2",
-  reuse=false)
+  reuse=false,
+  size=(plt_width, plt_height))
 
 for (num, sim, app, pot, state) in configs
 
@@ -97,13 +105,12 @@ for (num, sim, app, pot, state) in configs
       lw=2,
       color=p[cnt])
 
-    plot!(space * 1e3,
-      abs.(map(s -> potential(sim, app, pot, s), space)) / hbar,
-      lw=1,
-      ls=:dot,
-      label=nothing,
-      color=p[cnt])
-    display(space)
+    # plot!(space * 1e3,
+    #   abs.(map(s -> potential(sim, app, pot, s), space)) / hbar,
+    #   lw=1,
+    #   ls=:dot,
+    #   label=nothing,
+    #   color=p[cnt])
   else
     @printf("Estimated memory consumption (%4.1f MiB) exceed maximum!\n", estimate / (1024^2))
   end
