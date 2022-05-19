3118361f6b485a03fa59872da7412b7f0a192d57
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-05-19 19:04:17 +0200

    fix: startup (tentative)

--- startup.jl
+++ startup.jl
@@ -1,4 +1,18 @@
 using Pkg
-Pkg.activate(SolitonBEC)
-using SolitonBEC
-@time include("./test/test_transmission.jl")
+pkg"add Distributed"
+pkg"add Elliptic"
+pkg"add FFTW"
+pkg"add FileIO"
+pkg"add PhysicalConstants"
+pkg"add PlotlyBase"
+pkg"add Plots"
+pkg"add Printf"
+pkg"add ProgressMeter"
+pkg"add QuadGK"
+pkg"add LinearAlgebra"
+pkg"add Random"
+pkg"add SparseArrays"
+pkg"add Test"
+pkg"precompile"
+include("./src/SolitonBEC.jl")
+include("./test/test_transmission.jl")
