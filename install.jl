d56cad9f4d6650b0e0bb0aa5adf6eb935a15e9ac
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-05-20 13:13:21 +0200

    add: Pkg.instantiate (install dependencies)

--- install.jl
+++ install.jl
@@ -2,3 +2,4 @@ using Pkg
 Pkg.add(url="https://github.com/lorenzifrancesco/soliton-BEC")
 Pkg.checkout("SolitonBEC", "transmission-exploration")
 Pkg.activate("SolitonBEC")
+Pkg.instantiate("SolitonBEC")
