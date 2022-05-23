88005824b69ce4627c3b1cd2e4fa5cdf82edbed7
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-05-23 19:31:41 +0200

    fix: keep profile low

--- test/test_transmission.jl
+++ test/test_transmission.jl
@@ -104,7 +104,7 @@ Energy = GSEnergy + energy_unit * normd_vel^2*N/2
 
 ## ==================== Transmission grid configuration
 configs = []
-num_barr = 360
+num_barr = 216
 barrier_list = LinRange(0, 1, num_barr)
 velocity_list = LinRange(0, 1, num_barr)
 
