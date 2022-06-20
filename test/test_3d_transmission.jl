4715c5da3b0742a6a1bcfd5bf406c73aad834698
lorenzifrancesco <francesco.lorenzi.2@studenti.unipd.it>
2022-06-20 17:43:05 +0200

    change: increase barriers

--- test/test_3d_transmission.jl
+++ test/test_3d_transmission.jl
@@ -96,7 +96,7 @@ print("\n\tenergy unit: ", energy_unit, " J")
 ## ==================== Transmission grid configuration
 configs = []
 num_barr = 15
-barrier_list = LinRange(0, 15036/16, num_barr)
+barrier_list = LinRange(0, 15036/4, num_barr)
 velocity_list = LinRange(0, 1, num_barr)
 
 for vel in velocity_list
